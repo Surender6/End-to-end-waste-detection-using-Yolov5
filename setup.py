3d99ea40c98363028767f598f633fa43827b8e68
Surender6 <gudasurendar6@gmail.com>
2025-02-06 10:51:40 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='wasteDetection',
+    version='0.0.0',
+    author ='Guda Surender',
+    author_email = 'gudasurendar6@gmail.com',
+    packages =find_packages(),
+    install_requires = []
+) 
