f32a08a73866e7a46e55088390e2fda6ddad4f29
Surender6 <gudasurendar6@gmail.com>
2025-02-09 17:00:44 +0530

    Data ingestion added

--- wasteDetection/components/data_ingestion.py
+++ wasteDetection/components/data_ingestion.py
@@ -20,42 +20,43 @@ class DataIngestion:
         '''
         featch data from the url
         '''
-        try:
+        try: 
             dataset_url = self.data_ingestion_config.data_download_url
             zip_download_dir = self.data_ingestion_config.data_ingestion_dir
             os.makedirs(zip_download_dir, exist_ok=True)
             data_file_name = "data.zip"
             zip_file_path = os.path.join(zip_download_dir, data_file_name)
             logging.info(f"Downloading data from {dataset_url} into file {zip_file_path}")
-            
+
+
             file_id = dataset_url.split("/")[-2]
             prefix = 'https://drive.google.com/uc?/export=download&id='
-            logging.info(f"Download data from {dataset_url} into file {zip_file_path}")
-            
+            gdown.download(prefix+file_id,zip_file_path)
+
+            logging.info(f"Downloaded data from {dataset_url} into file {zip_file_path}")
+
             return zip_file_path
 
         except Exception as e:
-            raise AppException(e,sys)
+            raise AppException(e, sys)
         
-    def extract_zip_file(self,zip_file_path:str)-> str:
+    def extract_zip_file(self,zip_file_path: str)-> str:
         """
         zip_file_path: str
-        extracts the zipfile into the data directory 
-        function returns None 
-         
-        """ 
+        Extracts the zip file into the data directory
+        Function returns None
+        """
         try:
-           feature_store_path = self.data_ingestion_config.feature_store_file_path
-           os.makedirs(feature_store_path, exist_ok=True)
-           with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+            feature_store_path = self.data_ingestion_config.feature_store_file_path
+            os.makedirs(feature_store_path, exist_ok=True)
+            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
                 zip_ref.extractall(feature_store_path)
-           logging.info(f"Extracting zip file: {zip_file_path} into dir: {feature_store_path}")
+            logging.info(f"Extracting zip file: {zip_file_path} into dir: {feature_store_path}")
 
-            
-           return feature_store_path 
-           
+            return feature_store_path
+
         except Exception as e:
-            raise AppException(e,sys)    
+            raise AppException(e, sys)
             
     def initiate_data_ingestion(self) -> DataIngestionArtifact:
         logging.info("Entered initiate_data_ingestion method of Data_ingestion class") 
